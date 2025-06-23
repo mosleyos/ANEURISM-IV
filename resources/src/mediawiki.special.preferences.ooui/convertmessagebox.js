fa21e05bd6408d60ae61792089f5daa433337588
mosleyos <rosstikp88@gmail.com>
2025-06-23 22:34:33 +0300

    back

--- resources/src/mediawiki.special.preferences.ooui/convertmessagebox.js
+++
@@ -1,9 +0,0 @@
-/**
- * JavaScript for Special:Preferences: Check for successbox to replace with notifications.
- */
-( function () {
-	$( () => {
-		const convertmessagebox = require( 'mediawiki.notification.convertmessagebox' );
-		convertmessagebox();
-	} );
-}() );
